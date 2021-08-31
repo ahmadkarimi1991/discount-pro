fe410d7e7931fffb7b4956bb609a2706f06c8ae5
Ahmad <ahmadkarimi1991@gmail.com>
2021-09-01 00:52:34 +0430

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr" class="h-full">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Discount Pro">
+
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+  <title>Discount Pro</title>
+</head>
+
+<body class="min-h-full bg-gray-50 text-gray-900">
+  <div class="font-black text-center">Hi</div>
+</body>
+
+</html>
